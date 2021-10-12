5a6922d38d95b9e05a64469a34bf7843dcb83587
Fatih Serdar Karataş <54650968+fatihserdarkrts@users.noreply.github.com>
2021-10-12 22:47:20 +0300

    Add files via upload

---
+++ benimsinavim.com/js/queistons.js
@@ -0,0 +1,59 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "2 + 2 = ?",
+    answer: "4",
+    options: [
+      "8",
+      "4",
+      "6",
+      "12"
+    ]
+  },
+    {
+    numb: 2,
+    question: "4 * 4 = ?",
+    answer: "16",
+    options: [
+      "12",
+      "20",
+      "14",
+      "16"
+    ]
+  },
+    {
+    numb: 3,
+    question: " 2 + 2 * 5 ?",
+    answer: "12",
+    options: [
+      "12",
+      "20",
+      "15",
+      "10"
+    ]
+  },
+    {
+    numb: 4,
+    question: "10 * 2 - 20",
+    answer: "0",
+    options: [
+      "5",
+      "12",
+      "10",
+      "0"
+    ]
+  },
+    {
+    numb: 5,
+    question: "2 + 5 + 10",
+    answer: "17",
+    options: [
+      "10",
+      "22",
+      "17",
+      "25"
+    ]
+  },
+ 
+];
